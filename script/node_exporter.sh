3b12d807fc91c38e439612982fc61ca1a0c2cc9b
Gunjan Khanal <thisisgunjan@yahoo.com>
2024-08-16 14:05:28 +0545

    Revert "some files added"
    This reverts commit 50aa92d0fde439fef4866e93d91077b57c619caa.

--- script/node_exporter.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-sudo dnf -y update
-sudo dnf -y install wget
-link = "https://github.com/prometheus/node_exporter/releases/download/v1.8.2/node_exporter-1.8.2.linux-amd64.tar.gz"
-wget $link
-tar xvfz node_exporter-1.8.2.linux-amd64.tar.gz
-cd node_exporter-1.8.2.linux-amd64
-./node_exporter &
--- script/vmguest.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-sudo apt-get install -y build-essential linux-headers-$(uname -r)
-
-vagrant plugin install vagrant-vbguest
-vagrant up
